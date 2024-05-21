8b25a4f004d6518114bc85588074dcdd5533382a
SUBODH <snp42u@gmail.com>
2024-05-21 14:02:50 +0530

    Learned about Operation on Conversion

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "44"
 
- console.log(typeof score);
- console.log(typeof(score));
+//  console.log(typeof score);
+//  console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" => easily convert to number
 // "33abc" => NaN
@@ -13,7 +13,7 @@ console.log(typeof valueInNumber);
 let isloggedIn = "Romeo"
 
 let booleanIsloggedIn = Boolean(isloggedIn);
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -23,5 +23,43 @@ let sumNumber = 44
 
 let stringNumber = String (sumNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//********************** Operations ***************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello "
+let str2 = "Romeo"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
