c8996191b152bfe2e8d599a3c20d4927b77fc6e8
SUBODH <snp42u@gmail.com>
2024-05-21 12:49:41 +0530

    Conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "44"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => easily convert to number
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isloggedIn = "Romeo"
+
+let booleanIsloggedIn = Boolean(isloggedIn);
+console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Romeo" => true
+
+let sumNumber = 44
+
+let stringNumber = String (sumNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
