971c54448d37c55836c9b8ffea24f7ae33239b99
SUBODH <snp42u@gmail.com>
2024-05-29 00:08:26 +0530

    learn about hoisting

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -1,7 +1,44 @@
-let a =10
-const b =20
-var c=30
+let a = 10
+const b = 20
+var c = 30
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+    const username = "Romeo"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Romeo"
+    if (username === "Romeo") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// **************************** Intresting *******
+
+console.log(addOne(5)); 
+function addOne(num){
+    return num + 1
+}
+
+
+addTwo()
+const addTwo = function(num){
+    return num + 2
+}
