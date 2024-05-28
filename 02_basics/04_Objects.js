cc52e926d022ae170ff975ace2a828b45928fe27
SUBODH <snp42u@gmail.com>
2024-05-28 11:36:17 +0530

    learn about api

--- 02_basics/04_Objects.js
+++ 02_basics/04_Objects.js
@@ -52,10 +52,33 @@ const users = [
 
 users[1].email
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+// Destructing of objects
+
+
+const course = {
+    courseName: "js in hindi",
+    price:"999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: con} = course
+// console.log(courseInstructor);
+console.log(con);
+
+
+// {
+//     name: "Subodh",
+//     courseName:"js in hindi",
+//     price: "free"
+// }
+
