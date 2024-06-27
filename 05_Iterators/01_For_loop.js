f769def4ef61e04a37de12b643a5c03d1439a81e
SUBODH <snp42u@gmail.com>
2024-06-27 20:12:52 +0530

    name chane

---
+++ 05_Iterators/01_For_loop.js
@@ -0,0 +1,48 @@
+//for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and Inner loop ${i}`);
+
+        // console.log(i + "*" + j + " = " + i*j);
+        
+    }
+}
+
+
+
+let myArray = ["Flass", "Batman", "Superman", "Shaktiman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+
+//Break and Continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break;
+    }
+    console.log(`Value of i is ${index}`);
+}
+
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue;
+    }
+    console.log(`Value of i is ${index}`);
+}
